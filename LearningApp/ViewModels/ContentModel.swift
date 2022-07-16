6b72aa50e2b11e3762a8c08062e3e8f213ba9c26
Iggy <id@iggypacanowski.com>
2022-07-16 16:03:04 +0200

    organise file structure

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Iggy Pacanowski on 16/07/2022.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
