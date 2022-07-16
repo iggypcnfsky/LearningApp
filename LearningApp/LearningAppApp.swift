cf129b2ed9bd5c04e5d501991dd2c3537478a393
Iggy <id@iggypacanowski.com>
2022-07-16 15:57:30 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Iggy Pacanowski on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
